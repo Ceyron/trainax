910de6ca6b0ed76c60418c13a72ac465a97668af
Felix Koehler <f.koehler@tum.de>
2024-03-11 14:41:38 +0100

    Correct package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="pdequinox",
+    name="trainax",
     version="0.0.1",
     description="JAX-based abstract implementation of learning methodologies for autoregressive neural operators",
     author="Felix Koehler",
