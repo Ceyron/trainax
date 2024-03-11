e0c954642eb03beb39df4e419cc933f33fb3c32f
Felix Koehler <f.koehler@tum.de>
2024-03-11 13:57:37 +0100

    Add CI/CD

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="pdequinox",
+    version="0.0.1",
+    description="JAX-based abstract implementation of learning methodologies for autoregressive neural operators",
+    author="Felix Koehler",
+)
