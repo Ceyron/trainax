a4ad4fa87838355bae30cdbf8c8d238a02a9eee1
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-04-30 15:27:50 +0200

    Add docs to supervised rollout configuration

--- trainax/configuration/_supervised.py
+++ trainax/configuration/_supervised.py
@@ -26,6 +26,30 @@ class Supervised(BaseConfiguration):
         cut_bptt_every: int = 1,
         time_level_weights: Optional[Float[Array, "num_rollout_steps"]] = None,
     ):
+        """
+        General supervised (rollout) configuration.
+
+        Falls back to classical one-step supervised training for
+        `num_rollout_steps=1` (default).
+
+        Args:
+            num_rollout_steps (int): The number of time steps to
+                autoregressively roll out the model. During calling this
+                configuration, it requires a similarly long reference trajectory
+                to be available. Defaults to 1.
+            time_level_loss (BaseLoss): The loss function to use at
+                each time step. Defaults to MSELoss().
+            cut_bptt (bool): Whether to cut the backpropagation through time
+                (BPTT), i.e., insert a `jax.lax.stop_gradient` into the
+                autoregressive network main chain. Defaults to False.
+            cut_bptt_every (int): The frequency at which to cut the BPTT.
+                Only relevant if `cut_bptt` is True. Defaults to 1 (meaning
+                after each step).
+            time_level_weights (array[float], optional): An array of length
+                `num_rollout_steps` that contains the weights for each time
+                step. Defaults to None, which means that all time steps have
+                the same weight (=1.0).
+        """
         self.num_rollout_steps = num_rollout_steps
         self.time_level_loss = time_level_loss
         self.cut_bptt = cut_bptt
@@ -43,6 +67,29 @@ class Supervised(BaseConfiguration):
         ref_stepper: eqx.Module = None,  # unused
         residuum_fn: eqx.Module = None,  # unused
     ) -> float:
+        """
+        Evaluate the supervised (rollout) configuration on the given data.
+
+        The data is supposed to have as many time steps as the number of rollout
+        steps plus one. No `ref_stepper` or `residuum_fn` is needed.
+
+        Args:
+            stepper (eqx.Module): The stepper to use for the configuration. Must
+                have the signature `stepper(u_prev: PyTree) -> u_next: PyTree`.
+            data (PyTree): The data to evaluate the configuration on. This
+                should contain the initial condition and the target trajectory.
+            ref_stepper (eqx.Module): For compatibility with other
+                configurations; not used. (keyword-only argument) residuum_fn
+            residuum_fn (eqx.Module): For compatibility with other
+                configurations; not used. (keyword-only argument)
+        
+        Returns:
+            float: The loss value computed by this configuration.
+
+        Raises:
+            ValueError: If the number of snapshots in the trajectory is less than
+                the number of rollout steps plus one.
+        """
         # Data is supposed to contain both the initial condition and the target
         ic, trj = extract_ic_and_trj(data)
 
