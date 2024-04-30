287365b801eab4e82e4cd480a72fd0accdd05a5a
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-04-30 07:04:42 +0200

    Add docs

--- trainax/configuration/_base_configuration.py
+++ trainax/configuration/_base_configuration.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import equinox as eqx
+from jaxtyping import PyTree
 
 
 class BaseConfiguration(eqx.Module, ABC):
@@ -8,9 +9,27 @@ class BaseConfiguration(eqx.Module, ABC):
     def __call__(
         self,
         stepper: eqx.Module,
-        data,
+        data: PyTree,
         *,
         ref_stepper: eqx.Module = None,
         residuum_fn: eqx.Module = None,
     ) -> float:
+        """
+        Evaluate the configuration on the given data.
+
+        Args:
+
+            stepper (eqx.Module): The stepper to use for the configuration. Must
+                have the signature `stepper(u_prev: PyTree) -> u_next: PyTree`.
+            data (PyTree): The data to evaluate the configuration on. This
+                depends on the concrete configuration. In the most reduced case,
+                it just contains the set of initial states.
+            ref_stepper (eqx.Module): The reference stepper to use for some
+                configurations. (keyword-only argument)
+            residuum_fn (eqx.Module): The residuum function to use for some
+                configurations. (keyword-only argument)
+
+        Returns:
+            float: The loss value computed by this configuration.
+        """
         pass
