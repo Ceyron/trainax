083d7155fb7b58590d5db216514cfbb4577943bd
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-04-30 15:29:25 +0200

    Rename file to show how concrete the configuration is

--- trainax/configuration/__init__.py
+++ trainax/configuration/__init__.py
@@ -8,7 +8,7 @@ from ._base_configuration import BaseConfiguration
 from ._composite import CompositeLossConfiguratoin
 from ._diverted_chain import DivertedChain
 from ._diverted_chain_branch_one import DivertedChainBranchOne
-from ._mix_chain import MixChainPostPhysics
+from ._mix_chain_post_physics import MixChainPostPhysics
 from ._residuum import Residuum
 from ._supervised import Supervised
 
