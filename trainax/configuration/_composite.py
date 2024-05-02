9ff370f20ae95c4969f2de0c9595ad5ad324bbde
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-05-02 06:44:58 +0200

    Add docs

--- trainax/configuration/_composite.py
+++ trainax/configuration/_composite.py
@@ -7,6 +7,23 @@ class CompositeLossConfiguratoin(BaseConfiguration):
     configurations: list[BaseConfiguration]
     weights: list[float]
 
+    def __init__(
+        self,
+        configurations: list[BaseConfiguration],
+        weights: list[float],
+    ):
+        """
+        Compose configurations with respective weights.
+
+        Args:
+            configurations (list[BaseConfiguration]): The list of configurations
+                to compose.
+            weights (list[float]): The list of weights to apply to the
+                configurations.
+        """
+        self.configurations = configurations
+        self.weights = weights
+
     def __call__(
         self,
         stepper: Module,
@@ -15,6 +32,27 @@ class CompositeLossConfiguratoin(BaseConfiguration):
         ref_stepper: Module = None,
         residuum_fn: Module = None
     ) -> float:
+        """
+        Evaluate the composite configuration on the given data.
+
+        Based on the underlying configurations, `ref_stepper` or `residuum_fn`
+        or both have to be supplied (as keyword-only arguments).
+
+        Args:
+            stepper (Module): The stepper to use for the configuration. Must
+                have the signature `stepper(u_prev: PyTree) -> u_next: PyTree`.
+            data (PyTree): The data to evaluate the configuration on. This
+                depends on the concrete configuration. In the most reduced case,
+                it just contains the set of initial states.
+            ref_stepper (Module): The reference stepper to use for some
+                configurations. Defaults to None.
+            residuum_fn (Module): The residuum function to use for some
+                configurations. Defaults to None.
+
+        Returns:
+            float: The loss value computed by all configurations combined and
+                weighted.
+        """
         loss = sum(
             weight
             * conf(stepper, data, ref_stepper=ref_stepper, residuum_fn=residuum_fn)
