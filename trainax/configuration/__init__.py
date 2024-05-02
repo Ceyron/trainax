e976400bff89bc358d183c306381240d91bd0f3d
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-05-02 06:49:18 +0200

    Extend module docstring

--- trainax/configuration/__init__.py
+++ trainax/configuration/__init__.py
@@ -1,6 +1,16 @@
 """
 A configuration is a compute graph that is used to train an autoregressive
-neural operator.
+neural emulator. It can be purely data-driven like supervised (rollout) training
+(`Supervised`) allowing the data-generator to be turned off during training.
+Alternatively, the emulator and either the `ref_stepper` (used to create the
+data) or the `residuum_fn` (a discrete condition on two consecutive time steps)
+can be more deeply interwoven. A `ref_stepper` is needed for the `DivertedChain`
+configuration and its special case `DivertedChainBranchOne` as well as the
+`MixChainPostPhysics`. The `Residuum` configuration uses a `residuum_fn` to
+compute the loss.
+
+The `Composite` configuration allows to combine multiple configurations with
+respective weights.
 """
 
 
