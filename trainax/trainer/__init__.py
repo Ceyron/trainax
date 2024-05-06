eadccdbc477edac7f085fc4e1022e72529441659
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-05-06 06:45:44 +0200

    Rename for better reflection of name

--- trainax/trainer/__init__.py
+++ trainax/trainer/__init__.py
@@ -1,11 +1,11 @@
 """
 A trainer performs the optimization using a configuration with correct
 minibatching. Internally, it combines a `BaseConfiguration` with the correctly
-set up `TrajectorySubStacker` and subclasses the `BaseTrainer`.
+set up `TrajectorySubStacker` and subclasses the `BaseTrainer
 """
 
 
-from ._diverted_chain import DivertedChainBranchOneTrainer
+from ._diverted_chain_branch_one import DivertedChainBranchOneTrainer
 from ._residuum import ResiduumTrainer
 from ._supervised import SupervisedTrainer
 
