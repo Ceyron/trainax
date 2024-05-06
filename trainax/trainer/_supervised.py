ed2bae7679cc8c914ac5e2b9d1f909e380b3d82f
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-05-06 06:38:52 +0200

    Add docs for the supervised trainer

--- trainax/trainer/_supervised.py
+++ trainax/trainer/_supervised.py
@@ -1,7 +1,8 @@
 from typing import Optional
 
 import equinox as eqx
-from jaxtyping import Array, Float
+import optax
+from jaxtyping import Array, Float, PyTree
 
 from .._general_trainer import GeneralTrainer
 from .._mixer import TrajectorySubStacker
@@ -13,11 +14,11 @@ from ..loss import BaseLoss, MSELoss
 class SupervisedTrainer(GeneralTrainer):
     def __init__(
         self,
-        data_trajectories,
+        data_trajectories: PyTree[Float[Array, "num_samples trj_len ..."]],
         *,
         ref_stepper: eqx.Module = None,  # for compatibility
         residuum_fn: eqx.Module = None,  # for compatibility
-        optimizer,
+        optimizer: optax.GradientTransformation,
         callback_fn: Optional[BaseCallback] = None,
         num_training_steps: int,
         batch_size: int,
@@ -30,6 +31,72 @@ class SupervisedTrainer(GeneralTrainer):
         ] = None,
         do_sub_stacking: bool = True,
     ):
+        """
+        Supervised (rollout) trainig for an autoregressive neural emulator on a
+        collection of trajectories.
+
+        Classical one-step supervised learning is achieved with
+        `num_rollout_steps=1`.
+
+        Ensure that the `data_trajectories` trajectory length is at least as
+        long as `num_rollout_steps + 1` (the additional step is for the initial
+        condition).
+
+        Args:
+            data_trajectories (PyTree[Float[Array, "num_samples trj_len ..."]]):
+                The batch of trajectories to slice. This must be a PyTree of
+                Arrays who have at least two leading axes: a batch-axis and a
+                time axis. For example, the zeroth axis can be associated with
+                multiple initial conditions or constitutive parameters and the
+                first axis represents all temporal snapshots. A PyTree can also
+                just be an array. You can provide additional leafs in the
+                PyTree, e.g., for the corresponding constitutive parameters etc.
+                Make sure that the emulator has the corresponding signature.
+            ref_stepper (eqx.Module): For compatibility with other
+                configurations; not used. (keyword-only argument)
+            residuum_fn (eqx.Module): For compatibility with other
+                configurations; not used. (keyword-only argument)
+            optimizer (optax.GradientTransformation): The optimizer to use for
+                training. For example, this can be `optax.adam(LEARNING_RATE)`.
+                Also use this to supply an optimizer with learning rate decay,
+                for example `optax.adam(optax.exponential_decay(...))`. If your
+                learning rate decay is designed for a certain number of update
+                steps, make sure that it aligns with `num_training_steps`.
+                (keyword-only argument)
+            callback_fn (BaseCallback, optional): A callback to use during
+                training. Defaults to None. (keyword-only argument)
+            num_training_steps (int): The number of training steps to perform.
+                (keyword-only argument)
+            batch_size (int): The batch size to use for training. Batches are
+                randomly sampled across both multiple trajectories, but also over
+                different windows within one trajectory. (keyword-only)
+            num_rollout_steps (int): The number of time steps to
+                autoregressively roll out the model. During calling this
+                configuration, it requires an at least similarly long reference
+                trajectory to be available. Defaults to 1 (which is one-step
+                supervised training). (keyword-only argument)
+            time_level_loss (BaseLoss): The loss function to use at
+                each time step. Defaults to MSELoss(). (keyword-only argument)
+            cut_bptt (bool): Whether to cut the backpropagation through time
+                (BPTT), i.e., insert a `jax.lax.stop_gradient` into the
+                autoregressive network main chain. Defaults to False.
+                (keyword-only argument)
+            cut_bptt_every (int): The frequency at which to cut the BPTT.
+                Only relevant if `cut_bptt` is True. Defaults to 1 (meaning
+                after each step). (keyword-only argument)
+            time_level_weights (array[float], optional): An array of length
+                `num_rollout_steps` that contains the weights for each time
+                step. Defaults to None, which means that all time steps have the
+                same weight (=1.0). (keyword-only argument)
+            do_sub_stacking (bool): Whether to use sub-stacking. If `False`, then
+                the given reference trajectory will not be sliced into windows
+                of length `num_rollout_steps + 1`. Defaults to True.
+                (keyword-only argument)
+
+        Info:
+            * Under reverse-mode automatic differentiation memory usage grows
+                linearly with `num_rollout_steps`.
+        """
         trajectory_sub_stacker = TrajectorySubStacker(
             data_trajectories,
             sub_trajectory_len=num_rollout_steps + 1,  # +1 for the IC
