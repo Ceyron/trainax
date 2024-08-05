60528aeb38df6a6caf63c8f1df7e131f51825205
Felix Koehler <f.koehler@tum.de>
2024-08-05 11:05:20 +0200

    Setup.py no longer needed

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="trainax",
-    version="0.0.1",
-    description="JAX-based abstract implementation of learning methodologies for autoregressive neural operators",
-    author="Felix Koehler",
-)
